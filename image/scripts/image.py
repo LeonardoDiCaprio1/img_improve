cb69a0acd60e602893bd361866c341dc9cd19df4
LeonardoDiCaprio1 <111164958+LeonardoDiCaprio1@users.noreply.github.com>
2023-05-30 16:29:06 +0800

    Update image.py

--- image/scripts/image.py
+++ image/scripts/image.py
@@ -12,16 +12,15 @@ def compress_image(image):
     return compressed_image
 
 class ImageProcessor:
-    def __init__(self, bmodel_path, labels_path):
-        # 初始化图像处理器
+    def __init__(self):
         self.bridge = CvBridge()
         self.lock = threading.Lock()
         self.cv_image = None
         self.running = True
-        
+
     def process_image(self, msg):
         try:
-             # 将ROS图像消息转换为OpenCV图像
+            # 将ROS图像消息转换为OpenCV图像
             cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
             with self.lock:
                 self.cv_image = cv_image
@@ -47,9 +46,9 @@ class ImageProcessor:
         self.running = False
 
 def image_publisher():
-    # 初始化ROS节点
+     # 初始化ROS节点
     rospy.init_node('image_publisher', anonymous=True)
-    
+
     processor = ImageProcessor()
     # 订阅原始相机图像话题
     rospy.Subscriber('/camera/color/image_raw', Image, processor.process_image)
@@ -61,7 +60,6 @@ def image_publisher():
     rospy.spin()
 
     compress_thread.join()
-   
-    
+
 if __name__ == '__main__':
     image_publisher()
